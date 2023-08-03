882b9068011551e2103bea2f515ad64fd44dbc69
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-08-03 10:49:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const timer = document.getElementById("timer");
+var date = new Date();
+ setInterval(mytimer,1000); 
+function mytimer() {
+	var dt = new Date();
+	timer.innerText = date.getMonth()+"/"+ date.getDate()+"/"+date.getFullYear() +", "+ dt.toLocaleTimeString();
+}
+
